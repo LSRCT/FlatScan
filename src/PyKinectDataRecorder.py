6d5f952cedc41784866a302f3c49e18acbdff1be
Alex Netsch <alex.netsch97@gmail.com>
2023-01-18 16:41:05 +0100

    Faster recording by keeping images in RAM and saving in the end

--- src/PyKinectDataRecorder.py
+++ src/PyKinectDataRecorder.py
@@ -2,7 +2,7 @@ from pykinect2 import PyKinectV2
 from pykinect2 import PyKinectRuntime
 import numpy as np
 import time
-import matplotlib.pyplot as plt
+from PIL import Image
 import os
 
 import ctypes
@@ -48,6 +48,8 @@ class VideoRecorderRuntime:
         pygame.display.set_caption("Kinect for Windows v2 Video Recorder")
         # back buffer surface for getting Kinect depth frames, 24 bit color, width and height equal to the Kinect depth frame size
         self._surface_d = pygame.Surface((self.w_d, self.h_d), 0, 24)
+        self.rgb_list = []
+        self.depth_list = []
         self.setup_dir()
 
     def setup_dir(self):
@@ -65,9 +67,22 @@ class VideoRecorderRuntime:
     def save_images(self, img_rgb, img_depth):
         """ Save color and depth images to the current directory. """
         self.frame_count += 1
-        plt.imsave(f"{self.dir_name_rgb}/{self.frame_count:05d}.png", img_rgb)
-        plt.imsave(
-            f"{self.dir_name_depth}/{self.frame_count:05d}.png", img_depth, cmap="gray")
+        # plt.imsave(f"{self.dir_name_rgb}/{self.frame_count:05d}.png", img_rgb)
+        img_rgb = Image.fromarray(img_rgb, 'RGB')
+        img_rgb.save(f"{self.dir_name_rgb}/{self.frame_count:05d}.png")
+        # save as 16 bit png
+        # TUM dataset scales by 5 as well
+        img_depth = Image.fromarray(img_depth * 5, 'I;16')
+        img_depth.save(f"{self.dir_name_depth}/{self.frame_count:05d}.png")
+    
+    def add_images(self, img_rgb, img_depth):
+        self.rgb_list.append(img_rgb)
+        self.depth_list.append(img_depth)
+
+    def save_all_images(self):
+        for i in range(len(self.rgb_list)):
+            self.save_images(self.rgb_list[i], self.depth_list[i])
+        print("Saved all images")
 
     def draw_color_frame(self, frame, target_surface):
         target_surface.lock()
@@ -153,8 +168,13 @@ class VideoRecorderRuntime:
                 depth_to_color = self._kinect.get_depth_to_color_map()
                 reg_img = self.get_registered_image(
                     depth_to_color, color_frame)
+                # kinect is mirrored, so flip the image
+                depth_frame = depth_frame[:, ::-1]
+                reg_img = reg_img[:, ::-1]
 
-                self.save_images(reg_img, depth_frame)
+                # self.save_images(reg_img, depth_frame)
+                self.add_images(reg_img, depth_frame)
+                time.sleep(0.1)
 
             if self._kinect.has_new_depth_frame():
                 frame = self._kinect.get_last_depth_frame()
@@ -171,6 +191,7 @@ class VideoRecorderRuntime:
             self._clock.tick(60)
 
         # Close our Kinect sensor, close the window and quit.
+        self.save_all_images()
         self._kinect.close()
         pygame.quit()
 
